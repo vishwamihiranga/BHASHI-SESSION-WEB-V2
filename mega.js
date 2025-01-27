4b63c2cb0f7c8eacad24b9adc8f7376f3c788427
Vishwa Mihiranga <145928580+vishwamihiranga@users.noreply.github.com>
2025-01-27 14:30:29 +0545

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,25 +1,34 @@
 const mega = require("megajs");
+
 const auth = {
     email: 'teamchalah@gmail.com',
     password: 'CHALAH@1228@',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
-}
+};
 
 const upload = (data, name) => {
     return new Promise((resolve, reject) => {
         try {
-            const storage = new mega.Storage(auth, () => {
-                data.pipe(storage.upload({name: name, allowUploadBuffering: true}));
-                storage.on("add", (file) => {
+            const storage = new mega.Storage(auth, async () => {
+                // Ensure that the storage is initialized before attempting to upload
+                try {
+                    const file = await storage.upload({ name: name, allowUploadBuffering: true });
+                    
+                    // Get the file URL once uploaded
                     file.link((err, url) => {
-                        if (err) throw err;
-                        storage.close()
-                        resolve(url);
+                        if (err) {
+                            reject(`Error getting the file link: ${err}`);
+                        } else {
+                            storage.close();
+                            resolve(url); // Return the file URL
+                        }
                     });
-                });
+                } catch (err) {
+                    reject(`Error uploading file: ${err}`);
+                }
             });
         } catch (err) {
-            reject(err);
+            reject(`Error initializing storage: ${err}`);
         }
     });
 };
